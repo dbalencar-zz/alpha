c93ea008f9b9939238eabe301b89faab416cb2ee
dbalencar <dbalencar@gmail.com>
2013-07-10 10:33:43 -0400

    Add: Filter: Participante.

--- protected/models/Certidao.php
+++ protected/models/Certidao.php
@@ -84,13 +84,14 @@ class Certidao extends CActiveRecord
 	 * Retrieves a list of models based on the current search/filter conditions.
 	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
 	 */
-	public function search()
+	public function search($participante)
 	{
 		// Warning: Please modify the following code to remove attributes that
 		// should not be searched.
 
 		$criteria=new CDbCriteria;
 
+		$criteria->compare('participante_licitacao_id', $participante->id);
 		$criteria->compare('tp_Certidao',$this->tp_Certidao);
 		$criteria->compare('tp_Pessoa',$this->tp_Pessoa);
 		$criteria->compare('nu_Certidao',$this->nu_Certidao,true);
