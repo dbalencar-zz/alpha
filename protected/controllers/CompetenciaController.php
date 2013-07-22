377f1a7e306459ec36f3adaf65885176fe9ee738
Douglas Braga de Alencar <douglas.alencar@policiacivil.am.gov.br>
2013-07-22 17:49:34 -0400

    Fix: Permissions.

--- protected/controllers/CompetenciaController.php
+++ protected/controllers/CompetenciaController.php
@@ -17,7 +17,7 @@ class CompetenciaController extends Controller
 		return array(
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','abrir','fechar'),
-				'users'=>array('admin'),
+				'users'=>array('@'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
